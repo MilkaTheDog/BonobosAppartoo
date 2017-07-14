8c27ef184f61c000d3acd70f93f728b3fb563cdb
Abdelhamid Azzedine <aazzedine@hexaglobe.com>
2017-07-14 07:34:17 +0200

    login template override

--- src/AppBundle/Entity/Bonobo.php
+++ src/AppBundle/Entity/Bonobo.php
@@ -18,9 +18,58 @@ class Bonobo extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Bonobo")
+     * @ORM\JoinTable(name="friends",
+     *     joinColumns={@ORM\JoinColumn(name="a", referencedColumnName="id")},
+     *     inverseJoinColumns={@ORM\JoinColumn(name="b", referencedColumnName="id")}
+     * )
+     */
+    private $friends;
+
     public function __construct()
     {
         parent::__construct();
-        // your own logic
     }
+
+    /**
+     * Add friend
+     *
+     * @param \AppBundle\Entity\Bonobo $friend
+     *
+     * @return Bonobo
+     */
+    public function addFriend(\AppBundle\Entity\Bonobo $friend)
+    {
+        if (!$this->friends->contains($friend)) {
+            $this->friends->add($friend);
+            $friend->addFriend($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remove friend
+     *
+     * @param \AppBundle\Entity\Bonobo $friend
+     */
+    public function removeFriend(\AppBundle\Entity\Bonobo $friend)
+    {
+        if ($this->friends->contains($friend)) {
+            $this->friends->removeElement($friend);
+            $friend->removeFriend($this);
+        }
+    }
+
+    /**
+     * Get friends
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getFriends()
+    {
+        return $this->friends;
+    }
+
 }
