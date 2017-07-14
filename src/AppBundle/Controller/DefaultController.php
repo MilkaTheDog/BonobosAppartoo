59de1252d7e474c2e19dd1ee050dafb5a29ca1ee
Abdelhamid Azzedine <aazzedine@hexaglobe.com>
2017-07-14 12:18:17 +0200

    friend details page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,6 +9,34 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    private function addOrRemoveFriend($actionType, $user, $friend)
+    {
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        if ($actionType === "add") {
+
+            $user->addFriend($friend);
+        } else if ($actionType === "remove") {
+
+            $user->removeFriend($friend);
+        }
+
+        $userManager->updateUser($user);
+    }
+
+    private function checkAndExecuteForm($request, $user)
+    {
+        if ($request->isMethod('POST') && ($toXFriend = $request->request->get('toXFriend'))
+            && ($actionType = $request->request->get('actionType'))) {
+
+            if (!($toXFriend = $this->getDoctrine()->getRepository(Bonobo::class)->find($toXFriend)))
+                throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException("Friend not found");
+
+            $this->addOrRemoveFriend($actionType, $user, $toXFriend);
+        }
+    }
+
     /**
      * @Route("/", name="homepage")
      */
@@ -16,31 +44,32 @@ class DefaultController extends Controller
     {
         /** @var \AppBundle\Entity\Bonobo $user */
         $user = $this->getUser();
-        $bonoboRepo = $this->getDoctrine()->getRepository(Bonobo::class);
 
-        if ($request->isMethod('POST') && ($toXFriend = $request->request->get('toXFriend'))
-            && ($actionType = $request->request->get('actionType'))) {
-
-            if (!($toXFriend = $bonoboRepo->find($toXFriend)))
-                throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException("Friend not found");
-
-            /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
-            $userManager = $this->container->get('fos_user.user_manager');
-
-            if ($actionType === "add") {
-
-                $user->addFriend($toXFriend);
-            } else if ($actionType === "remove") {
-
-                $user->removeFriend($toXFriend);
-            }
-
-            $userManager->updateUser($user);
-        }
+        $this->checkAndExecuteForm($request, $user);
 
         return $this->render('default/main.html.twig', [
             'user' => $user,
-            'userList' => $bonoboRepo->findAll()
+            'userList' => $this->getDoctrine()->getRepository(Bonobo::class)->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/show/{id}", name="showUser")
+     */
+    public function showAction(Request $request, $id)
+    {
+        /** @var \AppBundle\Entity\Bonobo $user */
+        $user = $this->getUser();
+
+        if (!($toShow = $this->getDoctrine()->getRepository(Bonobo::class)->find($id)))
+            throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException("Bonobo not found");
+
+        $this->checkAndExecuteForm($request, $user);
+
+        return $this->render('default/show.html.twig', [
+            'user' => $user,
+            'toShow' => $toShow,
+            'isFriend' => $user->getFriends()->contains($toShow)
         ]);
     }
 }
